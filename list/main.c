fb7dd02728a38d6feff680cba508d69e813f1773
Данила Печенин <dpechenin@icloud.com>
2024-05-25 13:44:17 +0300

    full done

--- list/main.c
+++ list/main.c
@@ -1,14 +1,118 @@
-//
-//  main.c
-//  list
-//
-//  Created by Данила Печенин on 28.04.2024.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+Node* addElement(Node* head, int value, unsigned int position) {
+    Node  *new_node, *tmp;
+    int i = 0;
+
+    new_node = (Node*)malloc(sizeof(Node));
+    new_node->data = value;
+    new_node->next = NULL;
+
+    if(!head) {
+        return new_node;
+    }
+
+    if(!position) {
+        new_node->next = head;
+        return new_node;
+    }
+
+    tmp = head;
+    while(tmp->next && i < position-1) {
+        tmp = tmp->next;
+        i++;
+    }
+
+    if(!tmp->next) {
+        tmp->next = new_node;
+        return head;
+    }
+
+    new_node->next = tmp->next;
+    tmp->next = new_node;
+    return head;
+}
+
+void printList(Node *head) {
+    if(!head) {
+        printf("List is empty!");
+    }
+
+    while(head) {
+        printf("%d\t", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+Node* deleteNode(Node *head, unsigned int index) {
+    Node *elm = head, *tmp;
+    int i = 0;
+
+    if (!head) {
+        printf("List is empty");
+        return head;
+    }
+
+    if(!index) {
+        elm = head->next;
+        head->next = NULL;//!!!
+        free(head);
+        return elm;
+    }
+
+    while(elm && i < index-1) {
+        elm = elm->next;
+        i++;
+    }
+
+    if(!elm) {
+        return head;
+    }
+    if(!elm->next) {
+        return head;
+    }
+
+    tmp = elm->next;
+    if((elm->next)->next) {
+        elm->next = (elm->next)->next;
+        tmp->next = NULL;
+    }
+    else {
+        elm->next = NULL;
+    }
+    free(tmp);
+    return head;
+}
+
+void deleteList(Node **head) {
+    Node *next, *tmp = *head;
+
+    while(tmp) {
+        next = tmp->next;
+        free(tmp);
+        tmp = next;
+    }
+
+    *head = NULL;
+
+    printf("List was deleted!");
+}
+
+int main(void) {
+    Node *pMyList = NULL;
+    pMyList = addElement(pMyList, 3, 0);
+    pMyList = addElement(pMyList, 3, 1);
+    pMyList = addElement(pMyList, 5, 2);
+    printList(pMyList);
+    deleteNode(pMyList, 1);
+    printList(pMyList);
+    deleteList(&pMyList);
     return 0;
 }
